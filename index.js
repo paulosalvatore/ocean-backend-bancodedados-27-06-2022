f566c19845045ce9658605ab24ef9f7a7f5478e0
Paulo Salvatore <salvatoregames@gmail.com>
2022-06-27 09:16:01 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+
+// Informar para o Express que estamos usando JSON
+// no body das requisições
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+
+// Endpoint Read All - [GET] /herois
+app.get("/herois", function (req, res) {
+  res.send(herois.filter(Boolean));
+});
+
+// Endpoint Read by ID - [GET] /herois/:id
+app.get("/herois/:id", function (req, res) {
+  const id = req.params.id;
+
+  const item = herois[id - 1];
+
+  res.send(item);
+});
+
+// Endpoint Create - [POST] /herois
+app.post("/herois", function (req, res) {
+  // Acessa o nome do herói no corpo da requisição
+  const item = req.body.nome;
+
+  // Insere o nome na lista
+  herois.push(item);
+
+  res.send("Item inserido com sucesso!");
+});
+
+// Endpoint Update - [PUT] /herois/:id
+app.put("/herois/:id", function (req, res) {
+  // Obtemos o ID pela rota
+  const id = req.params.id;
+
+  // Pegamos o nome que foi enviado no corpo da requisição
+  const item = req.body.nome;
+
+  // Atualizamos a lista, na posição id - 1, pelo novo item
+  herois[id - 1] = item;
+
+  res.send("Item atualizado com sucesso.");
+});
+
+// Endpoint Delete - [DELETE] /herois/:id
+app.delete("/herois/:id", function (req, res) {
+  // Obtemos o ID pela rota
+  const id = req.params.id;
+
+  // Removemos a posição id - 1 da lista
+  delete herois[id - 1];
+
+  res.send("Item removido com sucesso.");
+});
+
+app.listen(3000, () =>
+  console.log("Servidor rodando em http://localhost:3000")
+);
